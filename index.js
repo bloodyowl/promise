8f606ba796efa34ceafc08ae419a9eb3ecff3ea4
Matthias Le Brun <mlbli@me.com>
2014-04-30 11:55:33 +0200

    refacto

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var klass = require("bloody-class")
-  , immediate = require("bloody-immediate")
-  , callbacks = require("./lib/callbacks")
-  
+var immediate = require("bloody-immediate")
+var callbacks = require("./lib/callbacks")
+
 var promise = module.exports = klass.extend({
   PENDING : 0,
   FULFILLED : 1,
@@ -11,38 +11,58 @@ var promise = module.exports = klass.extend({
   _isPromise : 1,
   constructor : function(fn){
     this.callbacks = callbacks.create(this)
-    if(typeof fn != "function") return this
+    if(typeof fn != "function") {
+      return this
+    }
     this.fn = fn
     immediate.call(fn, this)
   },
   then : function(fulfillCallback, errorCallback){
     var self = this
-      , returnedPromise = promise.create()
-    this.callbacks.push(fulfillCallback, 1, returnedPromise)
-    this.callbacks.push(errorCallback, 2, returnedPromise)
+    var returnedPromise = promise.create()
+
+    this.callbacks.push(
+      fulfillCallback,
+      1,
+      returnedPromise
+    )
+
+    this.callbacks.push(
+      errorCallback,
+      2,
+      returnedPromise
+    )
+
     this.callbacks.update()
     return returnedPromise
   },
   fulfill : function(value){
     var self = this
-    if(self.status) return
+    if(self.status) {
+      return
+    }
     self[self.status = self.FULFILLED] = value
     this.callbacks.update()
   },
   reject : function(reason){
     var self = this
-    if(self.status) return
+    if(self.status) {
+      return
+    }
     self[self.status = self.REJECTED] = reason
     this.callbacks.update()
   },
   from : function(value){
     var fulfilled = promise.create()
     if(value && value._isPromise && value.then) {
-      value.then(function(value){
-        fulfilled.fulfill(value)
-      }, function(reason){
-        fulfilled.reject(reason)
-      })
+      value.then(
+        function(value){
+          fulfilled.fulfill(value)
+        },
+        function(reason){
+          fulfilled.reject(reason)
+        }
+      )
       return fulfilled
     }
     fulfilled.fulfill(value)
@@ -55,32 +75,39 @@ var promise = module.exports = klass.extend({
   },
   all : function(array){
     var index = -1
-      , length = array.length
-      , results = Array(length)
-      , returnedPromise = promise.create()
-    
+    var length = array.length
+    var results = Array(length)
+    var resultsMap = Array(length)
+    var returnedPromise = promise.create()
+
     function failAll(reason){
       returnedPromise.reject(reason)
     }
-    
+
     function check(){
       var length = results.length
       while(--length > -1) {
-        if(!(length in results)) return
+        if(resultsMap[length] !== 1) {
+          return
+        }
       }
       returnedPromise.fulfill(results)
     }
-    
+
     while(++index < length) {
       (function(any, index){
         promise.from(any)
-          .then(function(value){
-            results[index] = value
-            check()
-          }, failAll)
+          .then(
+            function(value){
+              results[index] = value
+              resultsMap[index] = 1
+              check()
+            },
+            failAll
+          )
       })(array[index], index)
     }
-    
+
     return returnedPromise
   }
 })
--- test/promise.js
+++ test/promise.js
@@ -11,7 +11,7 @@ tape("promise", function(test){
         test.equal(p, r, "promise is passed to function")
         r.fulfill(1)
       })
-      
+
     p.then(function(value){
       var p2 = promise.create()
       test.equal(value, 1, "value is passed")
@@ -31,7 +31,7 @@ tape("promise", function(test){
     }, function(reason){
       test.equal(reason, "foo", "executes rejectCallback")
     })
-    
+
     p.then(function(){
       var p4 = promise.create()
       setTimeout(function(){
@@ -44,7 +44,7 @@ tape("promise", function(test){
     }, function(reason){
       test.equal(reason, 1, "executes rejectCallback")
     })
-    
+
     p.then(function(value){
       test.equal(value, 1, "multiple callbacks can be pushed")
       p.reject()
@@ -62,7 +62,7 @@ tape("promise", function(test){
     }, function(reason){
       test.equal(reason, "foo", "should execute rejectCallback on error")
     })
-  
+
     p.then(function(){
       throw "foo"
     })
@@ -76,24 +76,24 @@ tape("promise", function(test){
     }, function(reason){
       test.fail("shouldn't execute rejectCallback")
     })
-    
+
     var e = promise.from(p)
-    
+
     test.notEqual(p, e, "promise.from from promise returns new promise")
 
     e.then(function(value){
       test.equal(value, 1, "promise.from from promise gets value")
     })
-    
+
     var r = promise.from(1)
-    
+
       r.then(function(value){
         test.equal(value, 1, "promise.from from any value has value")
         test.ok(r.status & r.FULFILLED, "promise.from value is fulfilled")
       })
-      
+
     var t = promise.createRejected(2)
-      
+
       t.then(function(){
         test.fail("promise.createRejected shouldn't run successCallback")
       }, function(reason){
@@ -108,35 +108,37 @@ tape("promise", function(test){
 
 tape("promise.all", function(test){
 
-  test.plan(4)
-  
+  test.plan(6)
+
   var p = promise.create()
     , r = promise.create()
     , q = promise.create()
-    , all = promise.all([1, p, r])
+    , all = promise.all([1, p, r, void 0])
     , allReject = promise.all([q])
-  
+
   p.fulfill(2)
-  
+
   setTimeout(function(){
     r.fulfill(3)
   }, 300)
-  
+
   q.reject("nah")
-  
+
   all.then(function(array){
     test.equal(array[0], 1, "converts values to promises")
     test.equal(array[1], 2, "binds to promises")
     test.equal(array[2], 3, "binds to promises")
+    test.equal(array[3], void 0, "binds to promises")
+    test.equal(array.length, 4, "binds to promises")
   }, function(){
     test.fail("shouldn't run rejectCallback")
   })
-  
+
   allReject.then(function(){
     test.fail("shouldn't run successCallback")
   },function(reason){
     test.equal(reason, "nah", "runs rejectCallback")
   })
-  
-  
+
+
 })
